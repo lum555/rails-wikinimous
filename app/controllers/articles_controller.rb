b155fe8d14422167b542aa30fa0650da2b0d1bda
Tom Niblo <lolcust@live.co.uk>
2019-11-05 17:17:29 +0900

    Added all functionality, need to add styling.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,51 @@
+class ArticlesController < ApplicationController
+# frozen_string_literal: true
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(task_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :details, :completed)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
